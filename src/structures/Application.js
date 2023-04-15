56662624bfae4a082808bd3b5c40168ff0dca854
Glitch (glitch-hello-node) <none>
2023-04-15 22:48:34 +0000

    📇🍡 Checkpoint ./src/structures/Application.js:41556106/1178

--- src/structures/Application.js
+++ src/structures/Application.js
@@ -1,20 +1,35 @@
-const fs = require('fs');
+
+const fs = require('node:fs');
+const path = require('node:path');
+const { Collection, Client } = require('discord.js');
+const { DiscordjsTypeError, ErrorCodes } = require('discord.js/src/errors');
 
 class Application {
-  constructor() {
-    
+  
+  constructor({ client, commandPath, EventsPath }) {
+    this._patch(this, "client", client);    
+    this._patch(this, "commandPath", commandPath);    
+    this._patch(this, "EventsPath", EventsPath);    
   }
   
   async _patch(obj, key, value) {
     const Object_c = new Object();
     Object_c[key] = value;
-    const main = this
+    const main = this;
     Object.defineProperty(main, key.toString?.(), {
-      value: 
-    })
-  }
+      value: Object_c[key],
+      writeable: false
+    });
+  };
   
   async build() {
-    
-  }
+      if (!this.client) throw new DiscordjsTypeError(ErrorCodes.InvalidType, "Client", "is Missing parameters");
+      if (!this.commandPath) throw new DiscordjsTypeError(ErrorCodes.InvalidType, "commandPath", "is Missing parameters");
+      if (!this.EventsPath) throw new DiscordjsTypeError(ErrorCodes.InvalidType, "EventsPath", "is Missing parameters");
+      
+  };
+  
+  
 }
+
+module.exports = Application
