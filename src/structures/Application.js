b57f1e705aa7e75addba9382040115dc2b35f1fd
Glitch (glitch-hello-node) <none>
2023-04-15 22:36:40 +0000

    📻✊ Checkpoint ./package.json:34167823/704:41556106/1089 ./src/index.js:41556106/2 ./src/structures/Application.js:34167823/115:41556106/353 ./index.js:34167823/215:41556106/78

---
+++ src/structures/Application.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+class Application {
+  constructor() {
+    
+  }
+  
+  async _patch(obj, key, value) {
+    const Object_c = new Object();
+    Object_c[key] = value;
+    const main = this
+    Object.defineProperty(main, key.toString?.(), {
+      value: 
+    })
+  }
+  
+  async build() {
+    
+  }
+}
